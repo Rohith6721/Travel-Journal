5860a30eae1dde311cd63334dbce8d38332a11b2
Rohith6721 <aakula.rohith6@gmail.com>
2025-06-29 17:28:51 +0530

    Map all the properties

--- App.jsx
+++ App.jsx
@@ -4,31 +4,35 @@ import Entry from "./components/Entry";
 import data from "./data";
 
 const App = () => {
+  const entryElements = data.map((entry) => {
+    return (
+      <Entry
+        key={entry.id}
 
-  const entryElements = data.map((entry)=>{
-      return(
-        <Entry 
-      
-      img = {{
-        src:entry.img.src,
-        alt:entry.img.alt
-      }}
-      title={entry.title}
-      country={entry.country}
-      googleMapsLink={entry.googleMapsLink}
-      dates={entry.dates}
-      text={entry.text}
+        //To map over the data for some data 
+        // img={{
+        //   src: entry.img.src,
+        //   alt: entry.img.alt,
+        // }}
+        // title={entry.title}
+        // country={entry.country}
+        // googleMapsLink={entry.googleMapsLink}
+        // dates={entry.dates}
+        // text={entry.text}
+
+        //To map over the data for entire data set
+
+        entry={entry}
+
 
       />
-      )
-  }); 
+    );
+  });
 
   return (
     <>
       <Header />
-      <main className="container">
-        {entryElements}
-      </main>
+      <main className="container">{entryElements}</main>
     </>
   );
 };
